d30e08d8fea3a90534a4f0cda98caab0a4704cc2
felipegaiad <fpgaiad@gmail.com>
2018-03-04 17:58:22 -0300

    Test Json communication

--- app/src/main/java/br/com/fpgaiad/vmovies/ui/MainActivity.java
+++ app/src/main/java/br/com/fpgaiad/vmovies/ui/MainActivity.java
@@ -1,13 +1,15 @@
-package br.com.fpgaiad.vmovies;
+package br.com.fpgaiad.vmovies.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import br.com.fpgaiad.vmovies.R;
+import br.com.fpgaiad.vmovies.repository.MovieRepository;
+
 public class MainActivity extends AppCompatActivity {
 
-    public final static String BASE_TEXT = "Esta é a posição #";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,6 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(new ListAdapter(BASE_TEXT));
+        recyclerView.setAdapter(new ListAdapter(this, new MovieRepository().getMovieResponse().getResults()));
     }
 }
