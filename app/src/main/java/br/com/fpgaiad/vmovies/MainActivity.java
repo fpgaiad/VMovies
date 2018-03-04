6a4f68759ee29cb8aaeb8b6d26f8e5a0c857cfd5
felipegaiad <fpgaiad@gmail.com>
2018-03-04 15:31:41 -0300

    Test recyclerview structure

--- app/src/main/java/br/com/fpgaiad/vmovies/MainActivity.java
+++ app/src/main/java/br/com/fpgaiad/vmovies/MainActivity.java
@@ -2,12 +2,21 @@ package br.com.fpgaiad.vmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String BASE_TEXT = "Esta é a posição #";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new ListAdapter(BASE_TEXT));
     }
 }
