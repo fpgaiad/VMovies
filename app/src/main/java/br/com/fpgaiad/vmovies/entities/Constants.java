f61a4aa129af35ddebd78c706fee5e6dcc8d6b51
felipegaiad <fpgaiad@gmail.com>
2018-03-11 04:29:04 -0300

    Use BuildConfig.java to hide API_KEY

--- app/src/main/java/br/com/fpgaiad/vmovies/entities/Constants.java
+++ app/src/main/java/br/com/fpgaiad/vmovies/entities/Constants.java
@@ -1,6 +1,8 @@
 package br.com.fpgaiad.vmovies.entities;
 
 
+import br.com.fpgaiad.vmovies.BuildConfig;
+
 public class Constants {
     public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185/";
     public static final String MOST_POPULAR_URL = "&language=en-US&sort_by=popularity.desc";
@@ -8,6 +10,9 @@ public class Constants {
 
     private static final String QUERY_BASE_URL =
             "https://api.themoviedb.org/3/discover/movie?api_key=";
+    static final String YOUR_API_KEY = BuildConfig.API_KEY;
+
+
     //Create your own YOUR_API_KEY at: https://www.themoviedb.org/settings/api
-    public static final String BASE_URL_WITH_API_KEY = QUERY_BASE_URL + ApiKey.YOUR_API_KEY;
+    public static final String BASE_URL_WITH_API_KEY = QUERY_BASE_URL + YOUR_API_KEY;
 }
